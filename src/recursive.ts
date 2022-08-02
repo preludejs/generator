4420866004c88b19f7ffa349e86c595a12708e7f
Mirek Rusin <mirek@me.com>
2022-08-02 08:24:23 +0200

    fix(recursive): changing done cb order

--- src/recursive.ts
+++ src/recursive.ts
@@ -1,9 +1,9 @@
 /** @returns generator from recursively applying function to its result. */
 const recursive =
-  function* <T>(next: (value: T, index: number) => T, value: T, done: (previousValue: undefined | T, value: T, index: number) => boolean = () => false): Generator<T> {
+  function* <T>(next: (value: T, index: number) => T, value: T, done: (value: T, index: number, previousValue: undefined | T) => boolean = () => false): Generator<T> {
     let index = 0
     let previousValue: undefined | T = undefined
-    while (!done(previousValue, value, index++)) {
+    while (!done(value, index++, previousValue)) {
       yield value
       previousValue = value
       value = next(value, index) // eslint-disable-line no-param-reassign
