edf442514a2365a71479b621102957d635546d4e
Mirek Rusin <mirek@me.com>
2022-08-02 07:58:40 +0200

    feat(recursive): support done handler

---
+++ src/recursive.test.ts
@@ -0,0 +1,10 @@
+import * as G from './index.js'
+
+test('recursive', () => {
+  expect(G.pipe(
+    G.recursive(_ => _ + 1, 1, _ => _ === 10),
+    G.array
+  )).toEqual([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+  ])
+})
--- src/recursive.ts
+++ src/recursive.ts
@@ -1,10 +1,12 @@
 /** @returns generator from recursively applying function to its result. */
 const recursive =
-  function* <T>(next: (value: T, index: number) => T, value: T): Generator<T> {
-    let index = 1
-    while (true) {
+  function* <T>(next: (value: T, index: number) => T, value: T, done: (previousValue: undefined | T, value: T, index: number) => boolean = () => false): Generator<T> {
+    let index = 0
+    let previousValue: undefined | T = undefined
+    while (!done(previousValue, value, index++)) {
       yield value
-      value = next(value, index++) // eslint-disable-line no-param-reassign
+      previousValue = value
+      value = next(value, index) // eslint-disable-line no-param-reassign
     }
   }
 
