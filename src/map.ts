96acc4f3eebf8807edc97be60dd67be3104e2ea9
Mirek Rusin <mirek@me.com>
2022-08-06 12:57:04 +0200

    chore(infer): infer return type of map

--- src/map.ts
+++ src/map.ts
@@ -1,7 +1,7 @@
 /** @yields mapped values. */
 const map =
   <T, R>(mapping: (value: T, index: number) => R) =>
-    function* (values: Iterable<T>): Generator<R> {
+    function* (values: Iterable<T>) {
       let index = 0
       for (const value of values) {
         yield mapping(value, index++)
