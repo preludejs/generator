a14e0a69517161d9f3ddd2fd1a100aec85ba413a
Mirek Rusin <mirek@me.com>
2023-10-27 12:33:21 +0200

    refactor(cartesian-product): using new value type

--- src/cartesian-product.ts
+++ src/cartesian-product.ts
@@ -1,15 +1,15 @@
 import array from './array.js'
-import type { Iterated } from './prelude.js'
+import type { Value } from './prelude.js'
 
 /** @yields cartesian product pairs. */
 const cartesianProduct =
   <T, U extends undefined | Iterable<unknown>>(g?: U) =>
-    function* (h: Iterable<T>): Generator<[T, U extends undefined ? T : Iterated<U>]> {
+    function* (h: Iterable<T>): Generator<[T, U extends undefined ? T : Value<U>]> {
       const g_ = array(g ?? h)
       const h_ = g ? h : g_
       for (const a of h_) {
         for (const b of g_) {
-          yield [a, b] as [T, U extends undefined ? T : Iterated<U>]
+          yield [a, b] as [T, U extends undefined ? T : Value<U>]
         }
       }
     }
