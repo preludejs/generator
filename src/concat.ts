1a72000a0a7e7411ce6d37198081992ec9dc9eac
Mirek Rusin <mirek@me.com>
2023-10-27 12:35:09 +0200

    refactor(concat): using value type

--- src/concat.ts
+++ src/concat.ts
@@ -1,11 +1,11 @@
-import type { Iterated } from './prelude.js'
+import type { Value } from './prelude.js'
 
 /** @yields values from provided iterables. */
 const concat =
-  function* <Gs extends Iterable<unknown>[]>(valuesIterable: Gs): Generator<Iterated<Gs[number]>> {
-    for (const values of valuesIterable) {
-      for (const value of values) {
-        yield value as Iterated<Gs[number]>
+  function* <Args extends Iterable<unknown>[]>(...iterables: Args): Generator<Value<Args[number]>> {
+    for (const iterable of iterables) {
+      for (const value of iterable) {
+        yield value as Value<Args[number]>
       }
     }
   }
