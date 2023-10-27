c720cc782f53ebf1f3df82f974a7f17014ca2f92
Mirek Rusin <mirek@me.com>
2023-10-27 12:29:03 +0200

    refactor(first)!: support sorted first

--- src/first.ts
+++ src/first.ts
@@ -1,16 +1,33 @@
-/**
- * @returns first element.
- *
- * @throws {Error} if iterable is empty.
- *
- * @see maybeFirst for non-throwing variant.
- */
-const first =
+import sort from './sort.js'
+import type * as Cmp from '@prelude/cmp'
+
+const unsortedFirst =
   <T>(values: Iterable<T>): T => {
     for (const value of values) {
       return value
     }
     throw new Error('Not found.')
   }
+
+const sortedFirst =
+  <T>(cmp: Cmp.t<T>) =>
+    <U extends T>(values: Iterable<U>) =>
+      unsortedFirst(sort<T>(cmp)(values))
+
+/**
+ * If `cmp` is provided sorts values before returning first element, otherwise returns first element from values in order as is.
+ *
+ * @returns first element.
+ *
+ * @throws {Error} if iterable is empty.
+ *
+ * @see maybeFirst for non-throwing variant.
+ */
+
+export const first =
+  <T>(cmp?: Cmp.t<T>) =>
+    cmp ?
+      sortedFirst(cmp) :
+      unsortedFirst
 
 export default first
