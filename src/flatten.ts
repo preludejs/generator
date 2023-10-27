9b151f991a3d4afe50e412200fac51e5fed91688
Mirek Rusin <mirek@me.com>
2023-10-27 12:30:00 +0200

    refactor(flatten): simplify flatten implementation

--- src/flatten.ts
+++ src/flatten.ts
@@ -1,10 +1,8 @@
 /** @yields flattened values from provided iterable iterables. */
 const flatten =
-  function* <T>(valuesArray: Iterable<Iterable<T>>): Generator<T> {
-    for (const values of valuesArray) {
-      for (const value of values) {
-        yield value
-      }
+  function* <T>(iterables: Iterable<Iterable<T>>) {
+    for (const iterable of iterables) {
+      yield* iterable
     }
   }
 
