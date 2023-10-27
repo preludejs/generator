cc7d3d552ee880caf15e7b137512e0c72a926c7d
Mirek Rusin <mirek@me.com>
2023-10-27 12:31:38 +0200

    refactor!: simplify prelude

--- src/prelude.ts
+++ src/prelude.ts
@@ -1,21 +1,13 @@
 export * as Cmp from '@prelude/cmp'
 
-export type Generated<T> =
-  T extends Generator<infer U> ?
+export type Value<T> =
+  T extends Iterable<infer U> ?
     U :
-    never
-
-export type Iterated<T> =
-  T extends Iterator<infer U> ?
-    U :
-    never
+    T extends Iterator<infer U> ?
+      U :
+      never
 
 export type Defined<T> =
   T extends undefined ?
     never :
     T
-
-export type IterableResult<G> =
-  G extends Iterable<infer T> ?
-    T :
-    never
