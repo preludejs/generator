654e1c54a2e3ff799ffbc1dfa8650d411a5a0bbc
Mirek Rusin <mirek@me.com>
2023-10-27 12:52:59 +0200

    refactor(maybe-last)!: support sorted maybe last

--- src/maybe-last.ts
+++ src/maybe-last.ts
@@ -1,10 +1,28 @@
-const maybeLast =
+import sort from './sort.js'
+import type * as Cmp from '@prelude/cmp'
+
+export const maybeUnsortedLast =
   <T>(values: Iterable<T>): undefined | T => {
-    let last: undefined | T = undefined
+    let result: undefined | T = undefined
     for (const value of values) {
-      last = value
+      result = value
     }
-    return last
+    return result
   }
+
+export const maybeSortedLast =
+  <T>(cmp: Cmp.t<T>) =>
+    <U extends T>(values: Iterable<U>) =>
+      maybeUnsortedLast(sort<T>(cmp)(values))
+
+/**
+ * If `cmp` is provided sorts values before returning last element, otherwise returns last element from values in order as is.
+ * @yields last element
+ */
+export const maybeLast =
+  <T>(cmp?: Cmp.t<T>) =>
+    cmp ?
+      maybeSortedLast(cmp) :
+      maybeUnsortedLast
 
 export default maybeLast
