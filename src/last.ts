d771c35b52209da08962c2c42e4ec4702a20958e
Mirek Rusin <mirek@me.com>
2023-10-27 12:41:34 +0200

    refactor(last)!: support sorted last

--- src/last.ts
+++ src/last.ts
@@ -1,4 +1,7 @@
-const last =
+import sort from './sort.js'
+import type * as Cmp from '@prelude/cmp'
+
+export const unsortedLast =
   <T>(values: Iterable<T>): T => {
     let result: undefined | T = undefined
     let found = false
@@ -11,5 +14,20 @@ const last =
     }
     return result as T
   }
+
+export const sortedLast =
+  <T>(cmp: Cmp.t<T>) =>
+    <U extends T>(values: Iterable<U>) =>
+      unsortedLast(sort<T>(cmp)(values))
+
+/**
+ * If `cmp` is provided sorts values before returning last element, otherwise returns last element from values in order as is.
+ * @yields last element
+ */
+export const last =
+  <T>(cmp?: Cmp.t<T>) =>
+    cmp ?
+      sortedLast(cmp) :
+      unsortedLast
 
 export default last
