00477b2cd34ff804e3fbbc9826ced0d9187641b7
Mirek Rusin <mirek@me.com>
2023-10-27 12:59:10 +0200

    test(prime-factors): cosmetic test update

--- src/prime-factors.test.ts
+++ src/prime-factors.test.ts
@@ -1,13 +1,10 @@
 import * as G from './index.js'
 
 test.each([
-  [0],
-  [42],
-  [100],
-  [Number.MAX_SAFE_INTEGER]
+  0,
+  42,
+  100,
+  Number.MAX_SAFE_INTEGER
 ])('prime factors of %i', _ => {
-  expect(G.pipe(
-    G.primeFactors(_),
-    G.array
-  )).toMatchSnapshot()
+  expect(G.array(G.primeFactors(_))).toMatchSnapshot()
 })
